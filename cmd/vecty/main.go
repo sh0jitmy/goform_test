38bc009ff575ec52a4502a445bc05168f08634bf
shoji.tomoya <shjtmy@gmail.com>
2024-01-04 00:53:50 +0900

    Form Commit Test (Incomplete)

--- cmd/vecty/main.go
+++ cmd/vecty/main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hexops/vecty"
 	"github.com/hexops/vecty/elem"
 	"github.com/hexops/vecty/prop"
-	//"github.com/hexops/vecty/style"
+	"github.com/hexops/vecty/event"
+
+	//"github.com/patrickmn/go-cache"
+	//"github.com/go-co-op/gocron/v2"
 )
 
 func main() {
 	vecty.SetTitle("Vecty Form")
 	//flowbite from cdn
 	vecty.AddStylesheet("dist/css/flowbite.min.css")	
+	
+	// TODO.QueryClient Instance
+	/*
+	pg := &PageView{
+		queryclient : QueryClient{
+			endpoint :
+			sched : gocron.NewScheduler()
+			cache : cache.New(60*time.Second, 60*time.Second)
+		}
+	}
+	*/
+	fmt.Println("form start")
 	vecty.RenderBody(&PageView{})
 }
+
+/*
+type QueryClient {
+	endpoint string
+	cache cache.Cache
+}
+*/
 
 // PageView is our main page component.
 type PageView struct {
 	vecty.Core
+	se string //submit event
 }
 
 // Render implements the vecty.Component interface.
@@ -26,15 +50,30 @@ func (p *PageView) Render() vecty.ComponentOrHTML {
 		elem.Form(
 			vecty.Markup(
 				vecty.Class("max-w-sm"),vecty.Class("mx-auto"),
+				//event.Click(p.onSubmitHandle).PreventDefault(),
 			),
-			p.MakeLabelAndInput("Username",prop.TypeText),	
-			p.MakeLabelAndInput("Password",prop.TypePassword),	
-			p.MakeButton("Submit"),	
+			p.MakeLabelAndInput("Username",prop.TypeText,"kokusai taro"),	
+			p.MakeLabelAndInput("Password",prop.TypePassword,""),	
+			//p.MakeButton("Submit",nil),	
+			p.MakeButton("Submit",p.onSubmitHandle),	
+			vecty.Text(p.se),	
 		),	
 	)
 }
 
-func (p *PageView) MakeButton(labelname string) *vecty.HTML {
+func (p *PageView) onSubmitHandle(e *vecty.Event) {
+	//test code
+	p.se = "Form Submitted"	
+	//fmt.Println(e.Target.Get("value").String())
+	fmt.Println(e.Target.Length())
+	fmt.Println(e.Value.Length())
+	//fmt.Println(e.Target.Get("value").Get("Username").String())
+	//p.se = e.Value.Get("Username").String()	
+	//fmt.Println(p.se)
+	vecty.Rerender(p)	
+}
+
+func (p *PageView) MakeButton(labelname string,callback func(*vecty.Event)) *vecty.HTML {
 	return elem.Button(
 		vecty.Markup(
 			vecty.Class("text-white"),
@@ -54,12 +93,15 @@ func (p *PageView) MakeButton(labelname string) *vecty.HTML {
 			vecty.Class("dark:bg-blue-600"),
 			vecty.Class("dark:hover:bg-blue-700"),
 			vecty.Class("dark:focus:ring-blue-800"),
+			prop.Type(prop.TypeSubmit),
+			//event.Click(p.onSubmitHandle).PreventDefault(),
+			event.Click(callback).PreventDefault(),
 		),
 		vecty.Text(labelname),
 	);	
 }
 
-func (p *PageView) MakeLabelAndInput(labelname string,ptype prop.InputType) *vecty.HTML {
+func (p *PageView) MakeLabelAndInput(labelname string,ptype prop.InputType,plhd string) *vecty.HTML {
 	return elem.Div(
 		vecty.Markup(
 			vecty.Class("mb-5"),
@@ -69,7 +111,7 @@ func (p *PageView) MakeLabelAndInput(labelname string,ptype prop.InputType) *vec
 			vecty.Text(labelname),
 		),
 		elem.Input(
-			p.MarkupInput(ptype),	
+			p.MarkupInput(labelname,ptype,plhd),	
 		),
 	);
 }
@@ -85,7 +127,7 @@ func (p *PageView) MarkupLabel() vecty.MarkupList {
 	);
 }
 
-func (p *PageView) MarkupInput(ptype prop.InputType) vecty.MarkupList {
+func (p *PageView) MarkupInput(id string,ptype prop.InputType,plhd string) vecty.MarkupList {
 	return vecty.Markup(
 		vecty.Class("bg-gray-50"),
 		vecty.Class("border"),
@@ -105,6 +147,8 @@ func (p *PageView) MarkupInput(ptype prop.InputType) vecty.MarkupList {
 		vecty.Class("dark:focus:ring-blue-500"),
 		vecty.Class("dark:focus:border-blue-500"),
 		prop.Type(ptype),
+		prop.Name(id),
+		prop.Placeholder(plhd),
 	);
 }
 
